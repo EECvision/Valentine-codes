e25189e1922f7c9963ff1ef6beeb6226fafe2ea9
EECvision <eec.studies@gmail.com>
2023-01-16 21:57:21 +0100

    set up husky, prettier and eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react';
-import classes from './App.module.css';
-import Header from './components/Header/Header';
-
+import classes from "./App.module.css";
+import Header from "./components/Header/Header";
 
 function App() {
-
-    return (
-        <div className={classes.App}>
-            <Header />
-            <h1 className={classes.title}>Finance Budgeting</h1>
-        </div>
-    );
+  return (
+    <div className={classes.App}>
+      <Header />
+      <h1 className={classes.title}>Finance Budgeting</h1>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,11 @@
-
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
 
 function Header() {
-
-    return (
-        <div className={classes.Header}>
-            <h1 className={classes.title}>Save your money</h1>
-        </div>
-    );
+  return (
+    <div className={classes.Header}>
+      <h1 className={classes.title}>Save your money</h1>
+    </div>
+  );
 }
 
 export default Header;
