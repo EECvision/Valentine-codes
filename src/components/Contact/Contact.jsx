15474f59aa667ee082f5369e7039882188ac8ad0
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-01-19 10:07:51 +0100

    landing-page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,10 +4,62 @@ import classes from './Contact.module.css';
 function Contact() {
 
     return (
-        <div className={classes.Contactsection}>
-          <h1 className={classes.Contactheading}>Contact Us</h1>
-        </div>
+      <div className={classes.Contactsection}>
+      <div className={classes.Contacts}>
+      
+            <div className={classes.Subcontact}>
+                  <h3 className={classes.Productname}>Moneywise</h3>
+                  <h4 className={classes.Productdescription}>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h4>
 
+                  <div className={classes.Socialicons}>
+                      <span className={classes.icons}>
+                          <i className="fab fa-facebook-f" />
+                      </span>
+                      <span className={classes.icons}>
+                          <i className="fab fa-twitter" />
+                      </span>
+                      <span className={classes.icons}>
+                          <i className="fab fa-instagram" />
+                      </span>
+                      <span className={classes.icons}>
+                          <i className="fas fa-envelope" />
+                      </span>
+                 </div>
+            </div>
+
+            <div className={classes.Subcontact}>
+                  <h3 className={classes.Company}>Company</h3>
+                  <h4 className={classes.Companylist}>About Us</h4>
+                  <h4 className={classes.Companylist}>Features</h4>
+                  <h4 className={classes.Companylist}>Get started</h4>
+                  <h4 className={classes.Companylist}>FAQs</h4>
+            </div>
+
+            <div className={classes.Subcontact}>
+                  <h3 className={classes.Resources}>Resources</h3>
+                  <h4 className={classes.Resourceslist}>Excepteur</h4>
+                  <h4 className={classes.Resourceslist}>Excepteur</h4>
+                  <h4 className={classes.Resourceslist}>Excepteur</h4>
+                  <h4 className={classes.Resourceslist}>Excepteur</h4>
+            </div>
+
+            <div className={classes.Subcontact}>
+                  <h3 className={classes.Newsletter}>Join Our Newsletter</h3>
+
+                  <form>
+                  <div className={classes.formgroup} >
+                      <input type="text" className={classes.forminput} placeholder="Email address" required />
+                      <input className={classes.Signupbutton} type="submit" value="Subcribe" /> 
+                  </div>
+                  </form>
+
+                  {/* <h4 className={classes.Newsletterdescription}>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h4> */}
+              </div>
+
+        </div>
+        <p className={classes.Copyright}>Copyright © Moneywise 2023 </p>
+
+      </div>
 );
 }
 
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -6,44 +6,71 @@ function Features() {
     return (
 
     <section>
-    <div className={classes.Featuressection}>
+   <div className={classes.Featuremain}>
+    
+        <div className={classes.Featurebox}>
+          <span className={classes.iconstyle}>
+          <i className="fa-solid fa-circle-check" />
+          </span>
+          <h3 className={classes.Featureheading}>Lorem ipsum</h3>
+          <p className={classes.Featuretext}>Lacus vel facilisis volutpat est velit egestas dui. Elit sed vulputate mi sit amet mauris commodo quis.</p>
+        </div>
 
-        <div className={classes.Featuresimg}>
+        <div className={classes.Featurebox}>
+            <span className={classes.iconstyle}>
+            <i className="fa-solid fa-eye" />       
+            </span>
+          <h3 className={classes.Featureheading}>Lorem ipsum</h3>
+          <p className={classes.Featuretext}>Lacus vel facilisis volutpat est velit egestas dui. Elit sed vulputate mi sit amet mauris commodo quis.</p>
+        </div>
+        
+        <div className={classes.Featurebox}>
+          <span className={classes.iconstyle}>
+          <i className="fa-solid fa-laptop-code" />
+          </span>
+          <h3 className={classes.Featureheading}>Lorem ipsum</h3>
+          <p className={classes.Featuretext}>Lacus vel facilisis volutpat est velit egestas dui. Elit sed vulputate mi sit amet mauris commodo quis.</p>
+        </div>
+       </div>
+      
+    <div className={classes.Featuresection}>
+
+        <div className={classes.Featureimg}>
             <img className={classes.image} src="../src/assets/img02.png" alt=""/>
         </div>
  
-      <div className={classes.Featurescontent}>
-        <h3 className={classes.Features}>Features</h3>
-        <h2 className={classes.Featurestitle}>Excepteur sint occaecat cupidatat non proident.</h2>
-        <h4 className={classes.Featuressubtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco.</h4>
+      <div className={classes.Featurecontent}>
+        <h3 className={classes.Feature}>Features</h3>
+        <h2 className={classes.Featuretitle}>Excepteur sint occaecat cupidatat non proident.</h2>
+        <h4 className={classes.Featuresubtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco.</h4>
        </div>
        
     </div>
 
-    <div className={classes.Featuressection}>
+    <div className={classes.Featuresection}>
 
-      <div className={classes.Featurescontentmiddle}>
-        <h3 className={classes.Features}>Features</h3>
-        <h2 className={classes.Featurestitle}>Excepteur sint occaecat cupidatat non proident.</h2>
-        <h4 className={classes.Featuressubtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco.</h4>
+      <div className={classes.Featurecontentmiddle}>
+        <h3 className={classes.Feature}>Features</h3>
+        <h2 className={classes.Featuretitle}>Excepteur sint occaecat cupidatat non proident.</h2>
+        <h4 className={classes.Featuresubtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco.</h4>
        </div>
 
-       <div className={classes.Featuresimg}>
+       <div className={classes.Featureimage}>
             <img className={classes.image} src="../src/assets/img03.png" alt=""/>
         </div>
 
     </div>
 
-    <div className={classes.Featuressection}>
+    <div className={classes.Featuresection}>
 
-        <div className={classes.Featuresimg}>
+        <div className={classes.Featureimg}>
             <img className={classes.image} src="../src/assets/img04.png" alt=""/>
         </div>
 
-      <div className={classes.Featurescontent}>
-        <h3 className={classes.Features}>Features</h3>
-        <h2 className={classes.Featurestitle}>Excepteur sint occaecat cupidatat non proident.</h2>
-        <h4 className={classes.Featuressubtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco.</h4>
+      <div className={classes.Featurecontent}>
+        <h3 className={classes.Feature}>Features</h3>
+        <h2 className={classes.Featuretitle}>Excepteur sint occaecat cupidatat non proident.</h2>
+        <h4 className={classes.Featuresubtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco.</h4>
        </div>
 
     </div>
