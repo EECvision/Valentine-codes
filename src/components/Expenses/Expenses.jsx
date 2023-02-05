2480115b178425ec122650e181264e498b5b371f
EECvision <eec.studies@gmail.com>
2023-02-05 02:40:58 +0100

    improve dashboard, make expenses input functional

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,23 +1,63 @@
-import { Button } from "@mui/material";
-import { useState } from "react";
+import { Button, Select, InputLabel, MenuItem, FormControl } from "@mui/material";
+import { useEffect, useState } from "react";
 import ExpensesPopup from "../Popup/ExpensesPopup/ExpensesPopup";
 import classes from "./Expenses.module.css";
 
 const Expenses = () => {
-  // const [expenses, setExpenses] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+  const [filteredExpenses, setFilteredExpenses] = useState([]);
   const [togglePopup, setTogglePopup] = useState(false);
+  const [categories, setCategories] = useState(["Category A", "Category B", "Category C"]);
+  const [activeCategory, setActiveCategory] = useState("");
+
+  const handleSelectChange = (event) => {
+    setActiveCategory(event.target.value);
+    if (!event.target.value) return setFilteredExpenses(expenses);
+    const filtered = expenses.filter((exp) => exp.category === event.target.value);
+    setFilteredExpenses(filtered);
+  };
+
+  useEffect(() => {
+    setFilteredExpenses(expenses);
+  }, [expenses]);
 
   return (
     <div className={classes.container}>
-      {togglePopup ? <ExpensesPopup onClose={() => setTogglePopup(false)} /> : null}
+      {togglePopup ? (
+        <ExpensesPopup
+          setExpenses={setExpenses}
+          setCategories={setCategories}
+          categories={categories}
+          onClose={() => setTogglePopup(false)}
+        />
+      ) : null}
       <div className={classes.profile}></div>
       <div className={classes.control}>
         <Button onClick={() => setTogglePopup(true)} variant="contained">
           Add new expenses
         </Button>
-        <Button variant="contained">Filter section</Button>
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+          <InputLabel>Categories</InputLabel>
+          <Select value={activeCategory} onChange={handleSelectChange} label="Category">
+            <MenuItem value="">All</MenuItem>
+            {categories.map((category, idx) => (
+              <MenuItem key={idx} value={category}>
+                {category}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
       </div>
-      <div className={classes.expensesContainer}></div>
+      <div className={classes.expensesContainer}>
+        {filteredExpenses.map(({ expenses, amount, date, category }, idx) => (
+          <div key={idx} className={classes.expenses}>
+            <div>{expenses}</div>
+            <div>{amount}</div>
+            <div>{date}</div>
+            <div>{category}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Popup/ExpensesPopup/ExpensesPopup.jsx
+++ src/components/Popup/ExpensesPopup/ExpensesPopup.jsx
@@ -1,12 +1,124 @@
 import classes from "./ExpensesPopup.module.css";
+import CloseIcon from "@mui/icons-material/Close"; // add this to package.json file
+import IconButton from "@mui/material/IconButton";
+import { useEffect, useState } from "react";
+import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 
 /* eslint react/prop-types: 0 */
-const ExpensesPopup = ({ onClose }) => {
+const ExpensesPopup = ({ onClose, categories, setCategories, setExpenses: _setExpenses }) => {
+  const d = new Date();
+  const date = d.toString().split(" ").splice(0, 4).join(" ");
+
+  const [expenses, setExpenses] = useState({
+    expenses: "",
+    amount: "",
+    date,
+    category: "",
+  });
+  const [activeCategory, setActiveCategory] = useState("");
+  const [newCategory, setNewCategory] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  const handleSelectChange = (event) => {
+    setActiveCategory(event.target.value);
+    setExpenses((e) => ({ ...e, category: event.target.value }));
+    setNewCategory("");
+  };
+
+  const handleNewCategoryChange = (event) => {
+    setNewCategory(event.target.value);
+    setExpenses((e) => ({ ...e, category: event.target.value }));
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setExpenses((e) => ({ ...e, [name]: value }));
+  };
+
+  const handleSave = () => {
+    const expensesArr = Object.values(expenses);
+    const res = expensesArr.every((e) => e);
+    if (!res) return setIsError(true);
+    if (newCategory) {
+      setCategories((category) => [...category, newCategory]);
+    }
+    _setExpenses((e) => [...e, expenses]);
+    setExpenses({
+      expenses: "",
+      amount: "",
+      date: "",
+      category: "",
+    });
+    setActiveCategory("");
+    onClose(false);
+  };
+
+  useEffect(() => {
+    console.log({ newCategory });
+  }, [newCategory]);
+
   return (
     <div className={classes.container}>
       <div className={classes.wrapper}>
-        <div onClick={onClose}>close</div>
-        <div>This is a popup</div>
+        <div className={classes.closeIcon}>
+          <IconButton onClick={onClose} aria-label="close">
+            <CloseIcon />
+          </IconButton>
+        </div>
+        <div className={classes.heading}>Add your expenses</div>
+
+        <div className={classes.inputContainer}>
+          <TextField
+            error={!expenses.expenses && isError}
+            fullWidth="100%"
+            name="expenses"
+            label="expenses"
+            type="text"
+            variant="standard"
+            value={expenses.expenses}
+            onChange={handleInputChange}
+          />
+          <TextField
+            error={!expenses.amount && isError}
+            fullWidth="100%"
+            name="amount"
+            label="amount spent"
+            type="number"
+            variant="standard"
+            value={expenses.amount}
+            onChange={handleInputChange}
+          />
+          <div className={classes.date}>{date}</div>
+        </div>
+        <div className={classes.buttonContainer}>
+          <div>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 240 }}>
+              <InputLabel error={!activeCategory && isError}>Select an expense category</InputLabel>
+              <Select value={activeCategory} onChange={handleSelectChange} label="Category">
+                {categories.map((category, idx) => (
+                  <MenuItem key={idx} value={category}>
+                    {category}
+                  </MenuItem>
+                ))}
+                <MenuItem value="">Reset</MenuItem>
+              </Select>
+            </FormControl>
+            {!activeCategory && (
+              <>
+                <div>or</div>
+                <div className={classes.newCategory}>
+                  <div>create new</div>
+                  <TextField
+                    variant="standard"
+                    value={newCategory}
+                    onChange={handleNewCategoryChange}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <button onClick={handleSave}>+ Add</button>
+        </div>
       </div>
     </div>
   );
